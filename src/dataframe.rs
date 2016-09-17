3ee1aaed84102c6a739e5c29e534bb40810fdaf4
Eben Freeman <freemaneben@gmail.com>
2016-09-16 21:34:39 -0500

    test

--- src/dataframe.rs
+++ src/dataframe.rs
@@ -16,155 +16,153 @@ use ws::util::mask;
 /// don't allocate extra memory (again unlike the default Message implementation).
 #[derive(Debug, Clone, PartialEq)]
 pub struct DataFrame {
-	/// Whether or no this constitutes the end of a message
-	pub finished: bool,
-	/// The reserved portion of the data frame (RFC6455 5.2)
-	pub reserved: [bool; 3],
-	/// The opcode associated with this data frame
-	pub opcode: Opcode,
-	/// The payload associated with this data frame
-	pub data: Vec<u8>,
+    /// Whether or no this constitutes the end of a message
+    pub finished: bool,
+    /// The reserved portion of the data frame (RFC6455 5.2)
+    pub reserved: [bool; 3],
+    /// The opcode associated with this data frame
+    pub opcode: Opcode,
+    /// The payload associated with this data frame
+    pub data: Vec<u8>,
 }
 
 impl DataFrame {
-	/// Creates a new DataFrame.
-	pub fn new(finished: bool, opcode: Opcode, data: Vec<u8>) -> DataFrame {
-		DataFrame {
-			finished: finished,
-			reserved: [false; 3],
-			opcode: opcode,
-			data: data,
-		}
-	}
+    /// Creates a new DataFrame.
+    pub fn new(finished: bool, opcode: Opcode, data: Vec<u8>) -> DataFrame {
+        DataFrame {
+            finished: finished,
+            reserved: [false; 3],
+            opcode: opcode,
+            data: data,
+        }
+    }
 
     /// Reads a DataFrame from a Reader.
     pub fn read_dataframe<R>(reader: &mut R, should_be_masked: bool) -> WebSocketResult<Self>
-	where R: Read {
-    	let header = try!(dfh::read_header(reader));
+        where R: Read
+    {
+        let header = try!(dfh::read_header(reader));
 
-    	Ok(DataFrame {
-    		finished: header.flags.contains(dfh::FIN),
-    		reserved: [
-    			header.flags.contains(dfh::RSV1),
-    			header.flags.contains(dfh::RSV2),
-    			header.flags.contains(dfh::RSV3)
-    		],
-    		opcode: Opcode::new(header.opcode).expect("Invalid header opcode!"),
-    		data: match header.mask {
-    			Some(mask) => {
-    				if !should_be_masked {
-    					return Err(WebSocketError::DataFrameError(
-    						"Expected unmasked data frame"
-    					));
-    				}
+        Ok(DataFrame {
+            finished: header.flags.contains(dfh::FIN),
+            reserved: [header.flags.contains(dfh::RSV1),
+                       header.flags.contains(dfh::RSV2),
+                       header.flags.contains(dfh::RSV3)],
+            opcode: Opcode::new(header.opcode).expect("Invalid header opcode!"),
+            data: match header.mask {
+                Some(mask) => {
+                    if !should_be_masked {
+                        return Err(WebSocketError::DataFrameError("Expected unmasked data frame"));
+                    }
 
-    				let data: Vec<u8> = try!(reader.take(header.len).bytes().collect());
-    				mask::mask_data(mask, &data)
-    			}
-    			None => {
-    				if should_be_masked {
-    					return Err(WebSocketError::DataFrameError(
-    						"Expected masked data frame"
-    					));
-    				}
-
-    				try!(reader.take(header.len).bytes().collect())
-    			}
-    		}
-    	})
+                    let mut data: Vec<u8> = Vec::with_capacity(header.len as usize);
+                    let iter = reader.take(header.len).bytes();
+                    for b in iter {
+                        data.push(try!(b));
+                    }
+                    mask::mask_data(mask, &data)
+                }
+                None => {
+                    if should_be_masked {
+                        return Err(WebSocketError::DataFrameError("Expected masked data frame"));
+                    }
+                    try!(reader.take(header.len).bytes().collect())
+                }
+            },
+        })
     }
 }
 
 impl DataFrameable for DataFrame {
-	#[inline(always)]
+    #[inline(always)]
     fn is_last(&self) -> bool {
-		self.finished
-	}
+        self.finished
+    }
 
-	#[inline(always)]
+    #[inline(always)]
     fn opcode(&self) -> u8 {
-		self.opcode as u8
-	}
+        self.opcode as u8
+    }
 
-	#[inline(always)]
+    #[inline(always)]
     fn reserved<'a>(&'a self) -> &'a [bool; 3] {
-		&self.reserved
-	}
+        &self.reserved
+    }
 
-	#[inline(always)]
-	fn payload<'a>(&'a self) -> Cow<'a, [u8]> {
-		Cow::Borrowed(&self.data)
-	}
+    #[inline(always)]
+    fn payload<'a>(&'a self) -> Cow<'a, [u8]> {
+        Cow::Borrowed(&self.data)
+    }
 }
 
 /// Represents a WebSocket data frame opcode
 #[derive(Clone, Debug, Copy, PartialEq)]
 pub enum Opcode {
-	/// A continuation data frame
-	Continuation,
-	/// A UTF-8 text data frame
-	Text,
-	/// A binary data frame
-	Binary,
-	/// An undefined non-control data frame
-	NonControl1,
-	/// An undefined non-control data frame
-	NonControl2,
-	/// An undefined non-control data frame
-	NonControl3,
-	/// An undefined non-control data frame
-	NonControl4,
-	/// An undefined non-control data frame
-	NonControl5,
-	/// A close data frame
-	Close,
-	/// A ping data frame
-	Ping,
-	/// A pong data frame
-	Pong,
-	/// An undefined control data frame
-	Control1,
-	/// An undefined control data frame
-	Control2,
-	/// An undefined control data frame
-	Control3,
-	/// An undefined control data frame
-	Control4,
-	/// An undefined control data frame
-	Control5,
+    /// A continuation data frame
+    Continuation,
+    /// A UTF-8 text data frame
+    Text,
+    /// A binary data frame
+    Binary,
+    /// An undefined non-control data frame
+    NonControl1,
+    /// An undefined non-control data frame
+    NonControl2,
+    /// An undefined non-control data frame
+    NonControl3,
+    /// An undefined non-control data frame
+    NonControl4,
+    /// An undefined non-control data frame
+    NonControl5,
+    /// A close data frame
+    Close,
+    /// A ping data frame
+    Ping,
+    /// A pong data frame
+    Pong,
+    /// An undefined control data frame
+    Control1,
+    /// An undefined control data frame
+    Control2,
+    /// An undefined control data frame
+    Control3,
+    /// An undefined control data frame
+    Control4,
+    /// An undefined control data frame
+    Control5,
 }
 
 impl Opcode {
-	/// Attempts to form an Opcode from a nibble.
-	///
-	/// Returns the Opcode, or None if the opcode is out of range.
-	pub fn new(op: u8) -> Option<Opcode> {
-		Some(match op {
-			0 => Opcode::Continuation,
-			1 => Opcode::Text,
-			2 => Opcode::Binary,
-			3 => Opcode::NonControl1,
-			4 => Opcode::NonControl2,
-			5 => Opcode::NonControl3,
-			6 => Opcode::NonControl4,
-			7 => Opcode::NonControl5,
-			8 => Opcode::Close,
-			9 => Opcode::Ping,
-			10 => Opcode::Pong,
-			11 => Opcode::Control1,
-			12 => Opcode::Control2,
-			13 => Opcode::Control3,
-			14 => Opcode::Control4,
-			15 => Opcode::Control5,
-			_ => return None,
-		})
-	}
+    /// Attempts to form an Opcode from a nibble.
+    ///
+    /// Returns the Opcode, or None if the opcode is out of range.
+    pub fn new(op: u8) -> Option<Opcode> {
+        Some(match op {
+            0 => Opcode::Continuation,
+            1 => Opcode::Text,
+            2 => Opcode::Binary,
+            3 => Opcode::NonControl1,
+            4 => Opcode::NonControl2,
+            5 => Opcode::NonControl3,
+            6 => Opcode::NonControl4,
+            7 => Opcode::NonControl5,
+            8 => Opcode::Close,
+            9 => Opcode::Ping,
+            10 => Opcode::Pong,
+            11 => Opcode::Control1,
+            12 => Opcode::Control2,
+            13 => Opcode::Control3,
+            14 => Opcode::Control4,
+            15 => Opcode::Control5,
+            _ => return None,
+        })
+    }
 }
 
 #[cfg(all(feature = "nightly", test))]
 mod tests {
     use super::*;
-	use ws::dataframe::DataFrame as DataFrameable;
+    use ws::dataframe::DataFrame as DataFrameable;
     use test::Bencher;
 
     #[test]
@@ -179,53 +177,53 @@ mod tests {
             finished: true,
             reserved: [false; 3],
             opcode: Opcode::Text,
-            data: data.to_vec()
+            data: data.to_vec(),
         };
         assert_eq!(obtained, expected);
     }
     #[bench]
-	fn bench_read_dataframe(b: &mut Bencher) {
-		let data = b"The quick brown fox jumps over the lazy dog";
-		let mut dataframe = vec![0x81, 0x2B];
-		for i in data.iter() {
-			dataframe.push(*i);
-		}
-		b.iter(|| {
-			DataFrame::read_dataframe(&mut &dataframe[..], false).unwrap();
-		});
-	}
+    fn bench_read_dataframe(b: &mut Bencher) {
+        let data = b"The quick brown fox jumps over the lazy dog";
+        let mut dataframe = vec![0x81, 0x2B];
+        for i in data.iter() {
+            dataframe.push(*i);
+        }
+        b.iter(|| {
+            DataFrame::read_dataframe(&mut &dataframe[..], false).unwrap();
+        });
+    }
 
     #[test]
-	fn test_write_dataframe() {
-		let data = b"The quick brown fox jumps over the lazy dog";
-		let mut expected = vec![0x81, 0x2B];
-		for i in data.iter() {
-			expected.push(*i);
-		}
-		let dataframe = DataFrame {
-			finished: true,
-			reserved: [false; 3],
-			opcode: Opcode::Text,
-			data: data.to_vec()
-		};
-		let mut obtained = Vec::new();
+    fn test_write_dataframe() {
+        let data = b"The quick brown fox jumps over the lazy dog";
+        let mut expected = vec![0x81, 0x2B];
+        for i in data.iter() {
+            expected.push(*i);
+        }
+        let dataframe = DataFrame {
+            finished: true,
+            reserved: [false; 3],
+            opcode: Opcode::Text,
+            data: data.to_vec(),
+        };
+        let mut obtained = Vec::new();
         dataframe.write_to(&mut obtained, false).unwrap();
 
-		assert_eq!(&obtained[..], &expected[..]);
-	}
+        assert_eq!(&obtained[..], &expected[..]);
+    }
 
-	#[bench]
-	fn bench_write_dataframe(b: &mut Bencher) {
-		let data = b"The quick brown fox jumps over the lazy dog";
-		let dataframe = DataFrame {
-			finished: true,
-			reserved: [false; 3],
-			opcode: Opcode::Text,
-			data: data.to_vec()
-		};
-		let mut writer = Vec::with_capacity(45);
-		b.iter(|| {
-			dataframe.write_to(&mut writer, false).unwrap();
-		});
-	}
+    #[bench]
+    fn bench_write_dataframe(b: &mut Bencher) {
+        let data = b"The quick brown fox jumps over the lazy dog";
+        let dataframe = DataFrame {
+            finished: true,
+            reserved: [false; 3],
+            opcode: Opcode::Text,
+            data: data.to_vec(),
+        };
+        let mut writer = Vec::with_capacity(45);
+        b.iter(|| {
+            dataframe.write_to(&mut writer, false).unwrap();
+        });
+    }
 }
